332bc94e7a1e86ca5142389475dfbb6e0be779d2
Helly Nandu <hellynandu1208@gmail.com>
2025-11-25 17:44:25 +0530

    initial commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -221,6 +221,20 @@ export default function Home() {
           </div>
         </div>
       </section>
+
+<section className="min-h-screen flex flex-col items-center justify-center mt-0 pb-12 bg-white text-center space-y-6 px-4">
+  <h2 className="text-3xl md:text-4xl lg:text-5xl xl:text-6xl font-bold text-gray-700">
+Core Features
+  </h2>
+  <p>
+    Unlock the Potential of Innovation. Discover the Advanced AI Tools Transforming Your Ideas into Reality with Unmatched Precision and Intelligence.
+  </p>
+
+
+</section>
+
+
+
     </>
   );
 }
